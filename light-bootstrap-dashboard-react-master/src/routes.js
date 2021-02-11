78dc123b9b3bc378c2a2ab15e456325a834f713c
allan-moya <allan.s.moya@gmail.com>
2021-02-11 10:52:19 -0800

    modified side bar

--- light-bootstrap-dashboard-react-master/src/routes.js
+++ light-bootstrap-dashboard-react-master/src/routes.js
@@ -22,17 +22,10 @@ import Typography from "views/Typography.js";
 import Icons from "views/Icons.js";
 import Maps from "views/Maps.js";
 import Notifications from "views/Notifications.js";
-import Upgrade from "views/Upgrade.js";
+
 
 const dashboardRoutes = [
-  {
-    upgrade: true,
-    path: "/upgrade",
-    name: "Upgrade to PRO",
-    icon: "nc-icon nc-alien-33",
-    component: Upgrade,
-    layout: "/admin",
-  },
+
   {
     path: "/dashboard",
     name: "Dashboard",
@@ -54,20 +47,7 @@ const dashboardRoutes = [
     component: TableList,
     layout: "/admin",
   },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: "nc-icon nc-paper-2",
-    component: Typography,
-    layout: "/admin",
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "nc-icon nc-atom",
-    component: Icons,
-    layout: "/admin",
-  },
+
   {
     path: "/maps",
     name: "Maps",
